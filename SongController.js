a92f6c0dbc8df5dfb0439346be0037086221541b
Kristina Vegelius <kv@indentive.se>
2019-02-17 00:08:06 +0100

    changed var songs to a state and changed song.song to song.file

--- SongController.js
+++ SongController.js
@@ -4,8 +4,6 @@ import styles from './Style.js';
 import Sound from 'react-native-sound';
 import PickedSong from './PickedSong';
 import SongChoice from './SongChoice';
-import SongTree from './SongTree';
-
 
 const dimensions = Dimensions.get('window');
 const halfScreenHeight = Math.round(dimensions.height)/2;
@@ -20,21 +18,94 @@ class SongController extends React.Component {
     this.state = {
       current_song: null,
       isPlaying: false,
+      songs: [
+      {
+        "id": "trollmor",
+        "title": "Trollmor",
+        "img": require('./assets/trollmor.png'),
+        "file": "trollmor.mp3",
+        "top": 0,
+        "left": roundingPos,
+      },
+      {
+        "id": "vargsangen",
+        "title": "Vargsången",
+        "img": require('./assets/vargsangen.png'),
+        "file": "vargsangen.mp3",
+        "top": -roundingPos,
+        "left": squareWidth,
+      },
+      {
+        "id": "brahms_vaggvisa",
+        "title": "Brahms vaggvisa",
+        "img": require('./assets/brahms-vaggvisa.png'),
+        "file": "brahms_vaggvisa.mp3",
+        "top": 0,
+        "left": squareWidth*2-roundingPos,
+      },
+      {
+        "id": "blinka_lilla",
+        "title": "Blinka lilla",
+        "img": require('./assets/blinka-lilla.png'),
+        "file": "blinka_lilla.mp3",
+        "top": squareHeight,
+        "left": 0,
+      },
+      {
+        "id": "byssan_lull",
+        "title": "Byssan lull",
+        "img": require('./assets/byssan-lull.png'),
+        "file": "byssan_lull.mp3",
+        "top": squareHeight,
+        "left": squareWidth,
+      },
+      {
+        "id": "john_blund",
+        "title": "John Blund",
+        "img": require('./assets/john-blund.png'),
+        "file": "john_blund.mp3",
+        "top": squareHeight,
+        "left": squareWidth*2,
+      },
+      {
+        "id": "majas_visa",
+        "title": "Majas visa",
+        "img": require('./assets/majas-visa.png'),
+        "file": "majas_visa.mp3",
+        "top": squareHeight*2,
+        "left": roundingPos,
+      },
+      {
+        "id": "slumra_stilla",
+        "title": "Slumra stilla",
+        "img": require('./assets/slumra-stilla.png'),
+        "file": "slumra_stilla.mp3",
+        "top": squareHeight*2+roundingPos,
+        "left": squareWidth,
+      },
+      {
+        "id": "videvisan",
+        "title": "Videvisan",
+        "img": require('./assets/videvisan.png'),
+        "file": "videvisan.mp3",
+        "top": squareHeight*2,
+        "left": squareWidth*2-roundingPos,
+      }],
     }
 
   }
 
-  initSong(song){
+  initSong(file){
     if(this.state.current_song){
       sound.reset();
     } 
       
-    sound = new Sound(song, Sound.MAIN_BUNDLE, (error) => {
+    sound = new Sound(file, Sound.MAIN_BUNDLE, (error) => {
       if (error) {
         alert("Something wrong happened, when trying to initiate the song.");
       }
       // play when loaded
-      this.playSong(song);
+      this.playSong(file);
         
     });
 
@@ -47,92 +118,17 @@ class SongController extends React.Component {
     });
   }
 
-  playSong(song){
+  playSong(file){
     sound.play();
     this.setState({
       isPlaying: true,
-      current_song: song
+      current_song: file
     });
   }
 
   renderSongChoice() {
-    var songs = [
-      {
-        "id": "trollmor",
-        "title": "Trollmor",
-        "img": require('./assets/trollmor.png'),
-        "song": "trollmor.mp3",
-        "top": 0,
-        "left": roundingPos,
-      },
-      {
-        "id": "vargsangen",
-        "title": "Vargsången",
-        "img": require('./assets/vargsangen.png'),
-        "song": "vargsangen.mp3",
-        "top": -roundingPos,
-        "left": squareWidth,
-      },
-      {
-        "id": "brahms_vaggvisa",
-        "title": "Brahms vaggvisa",
-        "img": require('./assets/brahms-vaggvisa.png'),
-        "song": "brahms_vaggvisa.mp3",
-        "top": 0,
-        "left": squareWidth*2-roundingPos,
-      },
-      {
-        "id": "blinka_lilla",
-        "title": "Blinka lilla",
-        "img": require('./assets/blinka-lilla.png'),
-        "song": "blinka_lilla.mp3",
-        "top": squareHeight,
-        "left": 0,
-      },
-      {
-        "id": "byssan_lull",
-        "title": "Byssan lull",
-        "img": require('./assets/byssan-lull.png'),
-        "song": "byssan_lull.mp3",
-        "top": squareHeight,
-        "left": squareWidth,
-      },
-      {
-        "id": "john_blund",
-        "title": "John Blund",
-        "img": require('./assets/john-blund.png'),
-        "song": "john_blund.mp3",
-        "top": squareHeight,
-        "left": squareWidth*2,
-      },
-      {
-        "id": "majas_visa",
-        "title": "Majas visa",
-        "img": require('./assets/majas-visa.png'),
-        "song": "majas_visa.mp3",
-        "top": squareHeight*2,
-        "left": roundingPos,
-      },
-      {
-        "id": "slumra_stilla",
-        "title": "Slumra stilla",
-        "img": require('./assets/slumra-stilla.png'),
-        "song": "slumra_stilla.mp3",
-        "top": squareHeight*2+roundingPos,
-        "left": squareWidth,
-      },
-      {
-        "id": "videvisan",
-        "title": "Videvisan",
-        "img": require('./assets/videvisan.png'),
-        "song": "videvisan.mp3",
-        "top": squareHeight*2,
-        "left": squareWidth*2-roundingPos,
-      },
 
-    ];
-
-    const songsItems = songs.map((song) =>
+    const songsItems = this.state.songs.map((song) =>
       //TODO: This should be an own component, called song choice.
       <SongChoice 
         key={song.id} 
@@ -158,18 +154,18 @@ class SongController extends React.Component {
   }
   
   handleClick = (song) => {
-    if(song.song === this.state.current_song){
+    if(song.file === this.state.current_song){
       if(this.state.isPlaying){
         this.pauseSong();
         this.PickedSong.toggleShowHide(0, 0, null); 
       } 
       else{
-        this.playSong(song.song);
+        this.playSong(song.file);
         this.PickedSong.toggleShowHide(1, 1000, song.img);
       }
     }
     else{
-      this.initSong(song.song);
+      this.initSong(song.file);
       this.PickedSong.toggleShowHide(1, 1000, song.img);
     }
   }
